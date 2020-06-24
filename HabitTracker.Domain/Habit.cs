75600257042430607ee42e7f3bb6d02f2b7b885e
Giovanna Anacleto <giovanna18batalha@gmail.com>
2020-06-23 23:14:45 -0300

    Adding Classes - Habit, Work and .gitIgnore

---
+++ HabitTracker.Domain/Habit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HabitTracker.Domain
+{
+    public class Habit
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<DayOfWeek> HabitDays { get; set; }
+        public DateTime HabitTimeNotification { get; set; }
+
+    }
+}
---
+++ HabitTracker.Domain/Work.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitTracker.Domain
+{
+    public class Work
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime WorkDate { get; set; }
+
+
+
+    }
+}
